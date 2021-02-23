06b5f3ff0c42c5ec827ea6b15b0d1254cdfceabc
MCTixon <61108044+MCTixon@users.noreply.github.com>
2021-02-24 01:34:09 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var mySwiper = new Swiper('.photo_container', {
+    pagination: {
+     el: '.photo_pagination',
+     type: 'bullets',
+     bulletClass: 'photo_bullet',
+     bulletActiveClass: 'photo_bullet_active',
+     clickable: true
+    },
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+var mySwiper = new Swiper('.cert_container', {
+    pagination: {
+     el: '.cert_pagination',
+     type: 'bullets',
+     bulletClass: 'cert_bullet',
+     bulletActiveClass: 'cert_bullet_active',
+     clickable: true
+    }
+});
+var mySwiper = new Swiper('.club_container', {
+    pagination: {
+     el: '.club_pagination',
+     type: 'bullets',
+     bulletClass: 'club_bullet',
+     bulletActiveClass: 'club_bullet_active',
+     clickable: true
+    }
+});
